ba1f83214ae67a0caa95268c32a6efe511d3da42
Robert Laws <roblaws@me.com>
2024-02-07 09:03:52 +0300

    update home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,11 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>Calendar Sample One</Title>
+      <h1>Website to demo a Google Calendar API</h1>
       <Counter />
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+        Calendar data...
       </p>
     </main>
   );
